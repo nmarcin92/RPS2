8f0ddab4bf70ea22394004380b26e4e5fe2351f7
nmarcin92 <keguwfa8>
2015-11-05 17:23:33 +0100

    project structure

---
+++ rps-sensor/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='rps-sensor',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='mnowak',
+    author_email='mnowak@student.agh.edu.pl',
+    description='',
+    install_requires=['psutil']
+)
